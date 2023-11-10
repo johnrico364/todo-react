d82db6245f5610a8ab79452a48457aa6820fdfe6
John Anthony <johnrico364@gmail.com>
2023-11-10 21:16:45 +0800

    Updated code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="display-1">Learn React</div>
+      <i class="bi bi-android2"></i>
+    </div>
+  );
+}
+
+export default App;
