775dde50d7e7ed784c61d50bbd72674f30bc9624
John Anthony <johnrico364@gmail.com>
2023-11-15 23:18:53 +0800

    Finish code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,8 @@ import { useQuery } from "@tanstack/react-query";
 
 export const Home = () => {
   const navigate = useNavigate();
-  const { userData, setUpdateId, setUpdateTitle } = useContext(AppContext);
+  const { userData, updateId, setUpdateId, setUpdateTitle } =
+    useContext(AppContext);
 
   const getTodoAPI = async () => {
     try {
@@ -20,31 +21,80 @@ export const Home = () => {
       return err;
     }
   };
+  const statusAPI = async (newStatus, id) => {
+    try {
+      await axios.put(
+        `https://apex.oracle.com/pls/apex/jao_workspace/todo/todo/status/${id}`,
+        newStatus
+      );
+      return true;
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const data = useQuery({
     queryKey: ["todos"],
     queryFn: getTodoAPI,
+    refetchInterval: 1000,
   });
 
-  const toUpdate = (todoId, title) =>{
+  const toUpdate = (todoId, title) => {
     setUpdateId(todoId);
     setUpdateTitle(title);
-    navigate("/update")
-  }
+    navigate("/update");
+  };
+  const handleStatus = async (status, id) => {
+    console.log(status);
+    const data = {
+      status: status == true ? false : true,
+    };
+    await statusAPI(data, id);
+  };
 
   return (
     <div className="container-fluid" style={{ height: "100vh" }}>
       <div className="row justify-content-center ">
+        <div className="col-3 d-flex justify-content-center align-items-center">
+          <div className="animal-img"></div>
+        </div>
         <div className="col-5">
           <div className="todo-container text-center ">
             <div className="task-container overflow-auto">
               <span>ID: {userData.account_id}</span>
+              {data.isLoading && <h1>Loading...</h1>}
               {data.data?.map((d) => {
+                if (d.status === 0) {
+                  return (
+                    <div className="row justify-content-center">
+                      <div
+                        className="todo-task col-10"
+                        onClick={() => toUpdate(d.todo_id, d.title)}
+                      >
+                        {d.title}
+                      </div>
+                      <div className="col-1 pt-3">
+                        <i
+                          className="task-checkbox bi bi-clipboard2-check"
+                          onClick={() => handleStatus(d.status, d.todo_id)}
+                        ></i>
+                      </div>
+                    </div>
+                  );
+                }
                 return (
                   <div className="row justify-content-center">
-                    <div className="todo-task col-10" onClick={() => toUpdate(d.todo_id, d.title)}>{d.title}</div>
+                    <div
+                      className="task-finished todo-task col-10"
+                      onClick={() => toUpdate(d.todo_id, d.title)}
+                    >
+                      {d.title}
+                    </div>
                     <div className="col-1 pt-3">
-                      <i className="task-checkbox bi bi-clipboard2-check"></i>
+                      <i
+                        className="checkbox-finish task-checkbox bi bi-clipboard2-check"
+                        onClick={() => handleStatus(d.status, d.todo_id)}
+                      ></i>
                     </div>
                   </div>
                 );
@@ -59,6 +109,9 @@ export const Home = () => {
               className="btn-logout bi bi-box-arrow-in-left"
             ></i>
           </div>
+        </div>
+        <div className="col-3 d-flex justify-content-center align-items-center">
+          <div className="animal-img"></div>
         </div>
       </div>
     </div>
--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,27 +1,82 @@
 import { useNavigate } from "react-router-dom";
 import "../css/Update.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../App";
+import axios from "axios";
 
 export const Update = () => {
   const navigate = useNavigate();
-  const {updateId, updateTitle} = useContext(AppContext);
+  const { updateId, updateTitle, setUpdateTitle } = useContext(AppContext);
+
+  const updateTask = async (updatePost) => {
+    try {
+      await axios.put(
+        `https://apex.oracle.com/pls/apex/jao_workspace/todo/todo/${updateId}`,
+        updatePost
+      );
+      return true;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const deleteTask = async () => {
+    try {
+      axios.delete(
+        `https://apex.oracle.com/pls/apex/jao_workspace/todo/todo/${updateId}`
+      );
+      return true;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUpdate = async () => {
+    const data = {
+      title: updateTitle,
+    };
+
+    const status = await updateTask(data);
+
+    status && navigate("/home");
+  };
+  const handleDelete = async () => {
+    const status = await deleteTask();
+
+    status && navigate("/home");
+  };
 
   return (
     <div className="container-fluid" style={{ height: "100vh" }}>
       <div className="row justify-content-center ">
         <div className="col-5 text-white ">
           <div className="update-container text-center ">
-            <div className="update-task-container overflow-auto border">
+            <div className="update-task-container overflow-auto">
               <div className="row justify-content-center ">
                 <div className="col-7 mt-4 ">
-                  <input id="update-input" className="update-input w-100" type="text" placeholder="Update:"/>
+                  <input
+                    id="update-input"
+                    className="update-input w-100"
+                    type="text"
+                    placeholder="Update:"
+                    defaultValue={updateTitle}
+                    onChange={(e) => setUpdateTitle(e.target.value)}
+                  />
                 </div>
                 <div className="col-7 mt-4 ">
-                  <button className="btn-update-delete w-100">Update</button>
+                  <button
+                    className="btn-update-delete w-100"
+                    onClick={handleUpdate}
+                  >
+                    Update
+                  </button>
                 </div>
                 <div className="col-7 mt-4 ">
-                  <button className="btn-update-delete w-100">Delete</button>
+                  <button
+                    className="btn-update-delete w-100"
+                    onClick={handleDelete}
+                  >
+                    Delete
+                  </button>
                 </div>
               </div>
               {updateId} :{updateTitle}
